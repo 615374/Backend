c662af2153724cf27ec6b87a7a75367c1c825ec2
615374 <giselalanzillotta@gmail.com>
2023-08-16 22:09:36 -0300

    Pre entrega

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import multer from 'multer'
 import routerProd from './routes/products.routes.js'
 import {__dirname} from './path.js'
 
@@ -6,14 +7,31 @@ import {__dirname} from './path.js'
 const PORT = 4000
 const app = express()
 
+//Config
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => { //cb = callback
+        cb(null, 'src/public/img')
+
+    },
+    filename: (req,file,cb) => {
+        cb(null, `${Date.now()}${file.originalname}`) //concateno la fecha actual con el nombre del archivo
+    }
+})
+
 
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
+const upload = multer({storage: storage})
 
 //Routes
 app.use('/static', express.static(__dirname + '/public'))
 app.use('/api/product',routerProd)
+app.post('/upload', upload.single('product'), (req,res) =>{
+    console.log(req.file)
+    console.log(req.body)
+
+})
 
 //Server
 app.listen(PORT, () => {
