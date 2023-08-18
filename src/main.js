ee23c5d3b6d0b2545cc185aaff0057d12c87dde1
615374 <giselalanzillotta@gmail.com>
2023-08-17 22:26:59 -0300

    Pre-entrega + handlebars

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import multer from 'multer'
+import {engine} from 'express-handlebars'
 import routerProd from './routes/products.routes.js'
 import {__dirname} from './path.js'
+import path from 'path'
+
 
 
 const PORT = 4000
@@ -22,11 +25,22 @@ const storage = multer.diskStorage({
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.resolve(__dirname, './views'))
+
 const upload = multer({storage: storage})
 
 //Routes
 app.use('/static', express.static(__dirname + '/public'))
 app.use('/api/product',routerProd)
+app.get('/static', (req, res) => {
+    // Indica que plantilla voy a utilizar
+    res.render("home", {
+        nombreUsuario: "Gisela"
+
+    })
+})
 app.post('/upload', upload.single('product'), (req,res) =>{
     console.log(req.file)
     console.log(req.body)
