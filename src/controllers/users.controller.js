35dd24a6792f927463888ed3fdd1474937ee9b2e
615374 <giselalanzillotta@gmail.com>
2023-12-03 18:16:22 -0300

    Error Handler

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,28 +2,19 @@ import userModel from '../models/users.models.js';
 import CustomError from "../services/errors/customError.js";
 import EErrors from '../services/errors/enums.js';
 
-const postUser = async (req, res) => {
+const postUser = async (req, res, next) => {
     const { first_name, last_name, email, age } = req.body
-
-	if (!last_name || !first_name || !email) {
+    if (!last_name || !first_name || !email) {
 		CustomError.createError({
             name: "Error de creacion de usuario",
             cause: generateUserErrorInfo({ first_name, last_name, email }),
             message: "Una o mas propiedades estan incompletas o son invalidas",
             code: EErrors.MISSING_OR_INVALID_USER_DATA
         });
+        return res.status(400).send({ mensaje: 'Usuario existente' });
 	}
-    
-    try {
-		if (!req.user) {
-			return res.status(400).send({ mensaje: 'Usuario existente' });
-		}
-		res.status(200).send({ mensaje: 'Usuario creado' });
-	} catch (error) {
-		res.status(500).send({ mensaje: `Error al crear el usuario ${error}` });
-	}
+    res.status(200).send({ mensaje: 'Usuario creado' });
 };
-    
 
 const getUser = async (req, res) => {
 	try {
